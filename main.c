0738da235dffb2e0d04828d13583b31453f90a3f
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-10 21:15:52 +0200

    Starting tasks

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - printing a prompt
+ * 
+ * @argc: counts the number of arguments
+ * @argv: An array of arguments
+ *
+ * Return: 0
+ */
+
+int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
+{
+    char *prompt = "(ZLShell) $ ";
+
+    /*To store the address f the buffer holding whatever was typed*/
+    char *lineptr
+    /*To store the alloacted size in bytes*/
+    size_t n = 0; 
+
+    printf("%s", prompt);
+    /*Stdin represents the stream which represents the source from which we want the function to get the data from. In our case the keyboard aka standard input*/
+    getline(&lineptr, &n, stdin);
+    printf("%s\n", lineptr);
+
+    free(lineptr); /*getline alloactes memory dynamically, so we have to free the memory*/
+    return (0);
+}
