1f26896b908c85599dcff1d38121bf4ac09daf05
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-10 22:13:49 +0200

    Added an exiting feature to main.c

--- main.c
+++ main.c
@@ -11,23 +11,31 @@
 
 int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
-    char *prompt = "(ZLShell) $ ";
+	char *prompt = "(ZLShell) $ ";
 
-    /*To store the address f the buffer holding whatever was typed*/
-    char *lineptr;
-    /*To store the alloacted size in bytes*/
-    size_t n = 0;
+	/*To store the address f the buffer holding whatever was typed*/
+	char *lineptr;
+	/*To store the alloacted size in bytes*/
+	size_t n = 0;
+	/*Variable that holds the return value of the getline function*/
+	ssize_t nchars_read;
 
-    /*Creating an infinite loop*/
-    while (1)
-    {
-    printf("%s", prompt);
-    /*Stdin is stream that represents the source we want the function to get the data from, standard input*/
-    getline(&lineptr, &n, stdin);
-    printf("%s\n", lineptr);
+	/*Creating an infinite loop*/
+	while (1)
+	{
+	printf("%s", prompt);
+	/*Stdin is stream that represents the source we want the function to get the data from, standard input*/
+	nchars_read = getline(&lineptr, &n, stdin);
+	/*Check if the getline function failed or reached EOF or user use C^ + D*/
+	if (nchars_read == -1)
+	{
+		printf("Exiting shell....\n");
+		return (-1);
+	}
+	printf("%s\n", lineptr);
 
-    free(lineptr); /*getline alloactes memory dynamically, so we have to free the memory*/
-    }
+	free(lineptr); /*getline alloactes memory dynamically, so we have to free the memory*/
+	}
 
-    return (0);
+	return (0);
 }
