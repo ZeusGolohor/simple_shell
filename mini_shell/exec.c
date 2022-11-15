e1e4811215d0c2d791d54de15d9d80b138e62727
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-15 21:00:16 +0200

    Added environment

--- mini_shell/exec.c
+++ mini_shell/exec.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 
-/**
- * main - execve example
- *
- * Return: Always 0.
- */
-int main(void)
+int execute(char *argv[])
 {
-    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+    *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+    pid_t child_status = 0;
 
     printf("Before execve\n");
-    if (execve(argv[0], argv, NULL) == -1)
+    switch(child_status = fork())
     {
-        perror("Error:");
+        case -1:
+            perror("Error.");
+            exit(99);
+        case 0:
+            if (execve(argv[0], argv, NULL) == -1)
+                perror("ERROR\n");
+                exit(99);
+            break;
+        default:
+            sleep(3);
+            break;
     }
     printf("After execve\n");
     return (0);
