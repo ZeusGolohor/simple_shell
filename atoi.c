46f8c168f12adf9791b12cb2c3fbf19846b4000a
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-21 16:56:41 +0200

    Added atoi

---
+++ atoi.c
@@ -0,0 +1,59 @@
+#include "zlshell.h"
+
+/**
+ * _numlen = Functions finds the length of number being passed
+ * 
+ * @n: Number being passed
+ * 
+ * Return: The length of the number
+ */
+int _numlen(int n)
+{
+    int numlen = 0;
+
+    while (n / 10 != 0)
+    {
+        numlen++;
+        n /= 10;
+    }
+    return (numlen);
+}
+
+/**
+ * _atoi - Function converts into to a string
+ * 
+ * @num: Integer to be printed
+ * 
+ * Return: String converted from int
+ */
+char *_atoi(int num)
+{
+    char *st;
+    int ct =0, place = 0, divider = 1000000000;
+    int len = _numlen(num);
+
+    st = malloc(len * sizeof(char) + 1);
+    if (!st)
+        return (NULL);
+
+    if (num < 10)
+    {
+        st[ct++] = num + '0';
+        st[ct] = '\0';
+        return (st);
+    }
+
+    while (divider)
+    {
+        place = (num / divider) % 10;
+        if (place != 0 || (len >= 0 && st[ct - 1] >= '0'))
+        {
+            st[ct] = place + '0';
+            ct++;
+            len--;
+        }
+        divider /= 10;
+    }
+    st[ct] = '\0';
+    return (st);
+}
