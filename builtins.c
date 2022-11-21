90565ab4bf1dc96afe30d364c2067b690055f1b4
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-21 15:12:22 +0200

    Added builtins

---
+++ builtins.c
@@ -0,0 +1,48 @@
+#include "zlshell.h"
+
+/**
+ * shell_exit - exit current process
+ * 
+ * @args: arguments
+ * @input: input
+ * 
+ * Return: void
+ */
+void shell_exit(char **args, char *input)
+{
+	unsigned int l = 0;
+
+	if (input)
+		free(input);
+	if (args)
+	{
+		while (args[1 ])
+		{
+			if(args[l])
+				free(args[l]);
+			l++;
+		}
+		free(args);
+	}
+	pth_free(the_path);
+	pth_free(enviroment);
+	exit(0);
+}
+
+/**
+ * print_environment - Prints the environment
+ * 
+ * Return: void
+ */
+void print_environment(void)
+{
+	path_t *node;
+
+	node = environment;
+	while (node)
+	{
+		write(1, node->ptr, _strlen(node->ptr));
+		write(1, "\n", 1);
+		node = node->next;
+	}
+}
