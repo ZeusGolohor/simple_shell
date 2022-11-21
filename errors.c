5e6fcec457cffae7f50256b39b167f9407a3d7fa
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-21 22:27:51 +0200

    correction on errors.c

--- errors.c
+++ errors.c
@@ -13,7 +13,7 @@ void errenous(char **args, char *cmd, int errno)
 {
 	char *str;
 
-	str = atoi(line_count);
+	str = (&line_count);
 	write(STDOUT_FILENO, cmd, _strlen(cmd));
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, str, _strlen(str));
