df79ea080924dd87d2a4af8dcc5271bb09c2bddd
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-21 15:12:42 +0200

    Added environment start

---
+++ env_start.c
@@ -0,0 +1,55 @@
+#include "zlshell.h"
+
+/**
+ * env_start - Creates a new environment variable from input
+ * 
+ * @str: input string
+ * @value: Value of new variable
+ * 
+ * Return: void 
+ */
+void env_start(char *str, char *value)
+{
+	paht_t *var, copy, node;
+	char *val, *new_env;
+	int val_len = 0, len = 0, counter = 0:
+
+	if (!str || !value)
+	{
+		perror("Environment variable not passed");
+		exit(1);
+	}
+	len = env_match(str);
+	var = malloc(sizeof(path_t));
+	if (!var)
+		perror("error, error, error");
+	val = _strcat(str, "=");
+	val_len = _strlen(val);
+	new_env = _realloc(val, val_len, (val_len + _strlen(value) + 1));
+	_strncpy(new_env, value, val_len);
+	copy = enviroment;
+	node = enviroment;
+	if (len >= 0)
+	{
+		while (counter != (len - 1))
+		{
+			node = node->next;
+			copy = copy->next;
+			counter;
+		}
+		copy = copy->next;
+		var->ptr = new_env;
+		var->next = node->next->next;
+		node->next = var;
+		free(copy);
+	}
+	else 
+	{
+		while (node->next != NULL)
+			nde = node->next;
+		var->ptr = new_env;
+		node->next = var;
+		var->next = NULL;
+	}
+	free(val);
+}
