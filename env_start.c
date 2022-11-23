e609628e253dcc45555f3d7f94ca0484f7267240
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2022-11-23 16:28:19 +0200

    Re-fix on env_start.c

--- env_start.c
+++ env_start.c
@@ -13,7 +13,7 @@ void env_start(char *str, char *value)
 	path_t *var, *copy, *node;
 	char *val, *new_env;
 	int val_len = 0, len = 0, counter = 0;
-	
+
 	if (!str || !value)
 	{
 		perror("Environment variable not passed");
@@ -25,7 +25,7 @@ void env_start(char *str, char *value)
 		perror("error, error, error");
 	val = _strcat(str, "=");
 	val_len = _strlen(val);
-	new_env = realloc(val, (val_len + _strlen(value) + 1));
+	new_env = _realloc(val, val_len, (val_len + _strlen(value) + 1));
 	_strncpy(new_env, value, val_len);
 	copy = *environment;
 	node = *environment;
@@ -33,22 +33,22 @@ void env_start(char *str, char *value)
 	{
 		while (counter != (len - 1))
 		{
-			node = *node.next;
-			copy = *copy.next;
+			node = node->next;
+			copy = copy->next;
 			counter++;
 		}
-		copy = *copy.next;
+		copy = copy->next;
 		var->ptr = new_env;
-		var->next = node.next->next;
+		var->next = node->next->next;
 		node.next = var;
-		/* free(copy); */
+		free(copy);
 	}
 	else 
 	{
-		while (node.next != NULL)
-			node = *node.next;
+		while (node->next != NULL)
+			node = node.next;
 		var->ptr = new_env;
-		node.next = var;
+		node->next = var;
 		var->next = NULL;
 	}
 	free(val);
